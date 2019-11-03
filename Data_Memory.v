3124ad5c8fed9010db5dddc7d46ebd8c96561328
alvaro aguirre <44936171+chelaschill@users.noreply.github.com>
2019-11-03 10:11:31 -0500

    Update Data_Memory.v

--- Data_Memory.v
+++ Data_Memory.v
@@ -1,36 +1,69 @@
 module Data_Memory(clk, address, memwrite, writedata, read_data, memread);
 input clk;
 input address;
-input memwrite;
-input memread;
-input writedata;
+input memwrite; //control
+input memread; //control
+input writedata; 
 output read_data;
 
 reg[7:0]array[39:0];
 
 wire clk;
-wire memwrite;
-wire memread;
-wire[31:0] writedata;
-wire[31:0] address;
-reg[31:0] read_data;
+wire [31:0] address;
+wire [31:0] writedata;
+wire [1:0] memread;
+wire [1:0] memwrite;
+reg [31:0] read_data;
 
 initial
   begin
-    $readmemb("datamemory.txt", array);
+    $readmemb("array.txt", array);
   end 
 
-always @(*)
+always @(*) //REVISAR
 begin
-  if (memread == 1'b0)
-	if(memwrite == 1'b1)
-	begin
-	array[address] <= writedata;
-	end
-  else if(memread == 1'b1)
-	if(memwrite == 1'b0)
-	begin
-	read_data <= array[address];
-	end
+  if (memread == 2'b01) //lw
+  begin
+	read_data[31:24] <= array[address];
+	read_data[23:16] <= array[address+1];
+	read_data[15:8]  <= array[address+2];
+	read_data[7:0]  <=  array[address+3];
+  end
+  if(memread == 2'b10) //lb
+  begin
+    	read_data[7:0] <= array[address+3];
+	read_data[31:8] <= 24'hFFFFFF;
+  end
+  if(memread == 2'b11) //lh
+  begin
+    	read_data[7:0] <= array[address+3];
+	read_data[15:8] <= array[address+2];
+	read_data[31:16] <= 16'h0000;
+  end  
+end
+
+always@(negedge clk) //REVISAR
+begin
+  if (memwrite == 2'b01) //sw
+  begin 
+	array[address] <= read_data[31:24];
+	array[address+1] <= read_data[23:16];
+	array[address+2] <= read_data[15:8];
+	array[address+3] <= read_data[7:0];
+  end
+  if(memwrite == 2'b10) //sb
+  begin
+    	array[address+3] <= read_data[7:0];
+	array[address+2] <= 8'hFF;
+	array[address+1] <= 8'hFF;
+	array[address] <= 8'hFF;
+  end
+  if(memwrite == 2'b11) //sh
+  begin
+    	array[address+3] <= read_data[7:0];
+	array[address+2] <= read_data[15:8];
+	array[address+1] <= 8'h00;
+	array[address] <= 8'h00;
+  end 
 end
 endmodule
