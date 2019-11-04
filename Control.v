3c59a26d5041cfef9b0fea4a41ae0d3ce735a25f
cristhoperHL <cristhopermma@gmail.com>
2019-11-03 23:51:25 -0500

    editado

--- Control.v
+++ Control.v
@@ -5,6 +5,142 @@ output reg [1:0] ALUOp, Branch, MemRead, MemWrite;
 
 always @(*)
 begin
+if(Instruction==6'b000000)//R-Type
+  begin
+    RegDst =1;
+    Jump = 2'b00;
+    Branch = 2'b00;
+    MemRead = 2'b00;
+    MemtoReg = 0;
+    ALUOp = 2'b00;
+    MemWrite = 2'b00;
+    ALUSrc = 0;
+    RegWrite = 1;
+  end
+else
+  begin 
+  case(Instruction)
+  6'b000000: //R-type
+  begin
+    RegDst =1;
+    Jump = 2'b00;
+    Branch = 2'b00;
+    MemRead = 2'b00;
+    MemtoReg = 0;
+    ALUOp = 2'b00;
+    MemWrite = 2'b00;
+    ALUSrc = 0;
+    RegWrite = 1;
+  end
+
+  6'b100011: //lw
+  begin
+    RegDst = 0;
+    Jump = 2'b00;
+    Branch = 2'b00;
+    MemRead = 2'b01;
+    MemtoReg = 1;
+    ALUOp = 2'b01;	
+    MemWrite = 2'b00;
+    ALUSrc = 1;
+    RegWrite = 1;
+  end
+
+  6'b101011: //sw
+  begin
+    RegDst = 1'bx;
+    Jump = 2'b00;
+    Branch = 2'b00;
+    MemRead = 2'b00;
+    MemtoReg = 1'bx;
+    ALUOp = 2'b01;
+    MemWrite = 2'b01;
+    ALUSrc = 1;
+    RegWrite = 0;
+  end
+
+  6'b100000: //lb
+  begin
+    RegDst = 0;
+    Jump = 2'b00;
+    Branch = 2'b00;
+    MemRead = 2'b10;
+    MemtoReg = 1;
+    ALUOp = 2'b01;	
+    MemWrite = 2'b00;
+    ALUSrc = 1;
+    RegWrite = 1;
+  end
+
+  6'b101000: //sb
+  begin
+    RegDst = 1'bx;
+    Jump = 2'b00;
+    Branch = 2'b00;
+    MemRead = 2'b00;
+    MemtoReg = 1'bx;
+    ALUOp = 2'b01;	
+    MemWrite = 2'b10;
+    ALUSrc = 1;
+    RegWrite = 0;
+  end
+
+  6'b100001: //lh
+  begin
+    RegDst = 0;
+    Jump = 2'b00;
+    Branch = 2'b00;
+    MemRead = 2'b11;
+    MemtoReg = 1;
+    ALUOp = 2'b01;
+    MemWrite = 2'b00;
+    ALUSrc = 1;
+    RegWrite = 1;
+  end
+
+  6'b101001: //sh
+  begin
+    RegDst = 1'bx;
+    Jump = 2'b00;
+    Branch = 2'b00;
+    MemRead = 2'b00;
+    MemtoReg = 1'bx;
+    ALUOp = 2'b01;	
+    MemWrite = 2'b11;
+    ALUSrc = 1;
+    RegWrite = 0;
+  end
+  
+  6'b000100: //beq
+  begin
+    RegDst = 1'bx;
+    Jump = 2'b00;
+    Branch = 2'b01;
+    MemRead = 2'b00;
+    MemtoReg = 1'bx;
+    ALUOp = 2'b01;
+    MemWrite = 2'b00;
+    ALUSrc = 0;
+    RegWrite = 0;
+  end
+
+  6'b000101: //bneq
+  begin
+    RegDst = 1'bx;
+    Jump = 2'b00;
+    Branch = 2'b10;
+    MemRead = 2'b00;
+    MemtoReg = 1'bx;
+    ALUOp = 2'b01;
+    MemWrite = 2'b00;
+    ALUSrc = 0;
+    RegWrite = 0;
+  end
+  endcase
+  end
+end
+endmodule
+/*
 case(Instruction)
   6'b000000: //R-type
   begin
@@ -188,7 +324,7 @@ case(Instruction)
     RegWrite = 1;
   end*/
 
-  6'b100100: //and
+ /* 6'b100100: //and
   begin
     RegDst = 1;
     Jump = 2'b00;
@@ -331,6 +467,4 @@ case(Instruction)
     ALUSrc = 1;
     RegWrite = 1;
   end
-endcase
-end
- endmodule
+  */
