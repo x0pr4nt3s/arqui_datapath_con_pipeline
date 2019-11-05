1867ac5da96caffc16f8be3736c997940f9e4747
alvaro aguirre <44936171+chelaschill@users.noreply.github.com>
2019-11-05 17:24:58 -0500

    Update InstructionMemory.v

--- datapath/InstructionMemory.v
+++ datapath/InstructionMemory.v
@@ -2,12 +2,12 @@ module InstructionMemory(clk,pc,out);
 input clk;
 input [31:0] pc;
 output reg [31:0] out;
-  reg [7:0] IM [0:59];
+  reg [7:0] instrucciones [0:59];
 initial begin
   $readmemb("instrucciones.txt",instrucciones);
 end
 always@(pc)
 begin
-out <= {IM[pc],IM[pc+1],IM[pc+2],IM[pc+3]};
+  out <= {instrucciones[pc],instrucciones[pc+1],instrucciones[pc+2],instrucciones[pc+3]};
 end 
 endmodule
